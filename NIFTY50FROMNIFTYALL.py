027b0f6de8588b14cdb0bc893f9d5e559a0549b8
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2019-06-22 13:39:29 +0530

    Update NIFTY50FROMNIFTYALL.py

--- NIFTY50FROMNIFTYALL.py
+++ NIFTY50FROMNIFTYALL.py
@@ -6,31 +6,41 @@ import easygui
 from datetime import datetime
 from mysql.connector import Error
 from mysql.connector import errorcode
+import configparser
 
 '''
 This Script is for too Copy the Data of NiftyALL i.e the Bhavcopy Data to Nifty50Derived Table 
 which contain only Top 50 Nifty Symbol.
 '''
+
 class Nifty50FromNiftyAll:
     def copyData(self):
+
+        config_obj = configparser.ConfigParser()
+        config_obj.read("F:\\Python CSV\\1 Main Technical Analysis of National Stock Exchange\\Config\\Config.cfg")
+
         # DataBase Connection
-        print("Hellos")
+        print("Hellos from NiftyAll to Nifty50Derived ")
         try:
-            connection = mysql.connector.connect(host='localhost', database='Nifty', user='root', password='[Enter Password]')
+            connection = mysql.connector.connect(host=config_obj.get("Setting", "host"),
+                                                 database=config_obj.get("Setting", "database"),
+                                                 user=config_obj.get("Setting", "user"),
+                                                 password=config_obj.get("Setting", "password"))
             cursor = connection.cursor()
             # to check repeated record
             qry = "Select *  from NiftyAll"
             pdata = pd.read_sql_query(qry, connection)
-            
             if pdata.empty == True:
                 easygui.msgbox("NiftyAll table is Empty", title="Process Message")
                 if (connection.is_connected()):
                     cursor.close()
                     connection.close()
                 return
-            
-            sql_select_Query = "Select *  from NiftyAll  where Symbol = (Select DISTINCT SYMBOL FROM NIFTY50 WHERE NIFTY50.SYMBOL = NIFTYALL.SYMBOL)  and Series='EQ'"
-            cursor.execute(sql_select_Query)
+
+            sql_select_query = "Select *  from NiftyAll  where Symbol = (Select DISTINCT SYMBOL FROM NIFTY50 " \
+                               "WHERE NIFTY50.SYMBOL = NIFTYALL.SYMBOL)  and Series='EQ'"
+
+            cursor.execute(sql_select_query)
             records = cursor.fetchall()
             print(cursor)
             # Loop to store data
