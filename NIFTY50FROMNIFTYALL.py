a48c6a344b89bc1f18683fb2d7b76590f9d2e928
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2019-06-27 17:58:55 +0530

    Update NIFTY50FROMNIFTYALL.py

--- NIFTY50FROMNIFTYALL.py
+++ NIFTY50FROMNIFTYALL.py
@@ -3,7 +3,7 @@ import mysql.connector
 import os.path
 import pandas as pd
 import easygui
-from datetime import datetime
+import datetime
 from mysql.connector import Error
 from mysql.connector import errorcode
 import configparser
@@ -17,7 +17,7 @@ class Nifty50FromNiftyAll:
     def copyData(self):
 
         config_obj = configparser.ConfigParser()
-        config_obj.read("F:\\Python CSV\\1 Main Technical Analysis of National Stock Exchange\\Config\\Config.cfg")
+        config_obj.read("Y:\\Python CSV\\1 Main Technical Analysis of National Stock Exchange\\Config\\Config.cfg")
 
         # DataBase Connection
         print("Hellos from NiftyAll to Nifty50Derived ")
@@ -30,26 +30,59 @@ class Nifty50FromNiftyAll:
             # to check repeated record
             qry = "Select *  from NiftyAll"
             pdata = pd.read_sql_query(qry, connection)
+
             if pdata.empty == True:
                 easygui.msgbox("NiftyAll table is Empty", title="Process Message")
                 if (connection.is_connected()):
                     cursor.close()
                     connection.close()
                 return
+            last_day_of_nifty50 ="SELECT timestamp FROM Nifty50Derived WHERE Timestamp IN ( SELECT MAX( Timestamp ) " \
+                                 "FROM Nifty50Derived  )  Group by timestamp ORDER BY  timestamp ASC"
+            cursor.execute(last_day_of_nifty50)
+            last_day_of_nifty50 = cursor.fetchall()
 
-            sql_select_query = "Select *  from NiftyAll  where Symbol = (Select DISTINCT SYMBOL FROM NIFTY50 " \
-                               "WHERE NIFTY50.SYMBOL = NIFTYALL.SYMBOL)  and Series='EQ'"
 
-            cursor.execute(sql_select_query)
-            records = cursor.fetchall()
-            print(cursor)
-            # Loop to store data
-            for i in records:
-                cursor.execute(""" INSERT INTO Nifty50Derived VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
-                               (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12]))
+            if(len(last_day_of_nifty50)==0):
+                last_day_of_nifty50 = "SELECT timestamp FROM NiftyAll WHERE Timestamp IN ( SELECT MIN( Timestamp ) " \
+                                      "FROM NiftyAll  )  Group by timestamp ORDER BY  timestamp ASC"
+                cursor.execute(last_day_of_nifty50)
+                last_day_of_nifty50 = cursor.fetchall()
 
-            connection.commit()
-            easygui.msgbox("Data Already Present Record inserted successfully into Nifty50Derived table", title="Process Message")
+
+            last_day_of_niftyAll="SELECT timestamp FROM NiftyAll WHERE Timestamp IN ( SELECT MAX( Timestamp ) " \
+                                 "FROM NiftyAll  )  Group by timestamp ORDER BY  timestamp ASC"
+            cursor.execute(last_day_of_niftyAll)
+            last_day_of_niftyAll = cursor.fetchall()
+
+            last_day_of_nifty50 = last_day_of_nifty50[0][0]
+            last_day_of_niftyAll = last_day_of_niftyAll[0][0]
+
+            print(last_day_of_nifty50)
+            print(last_day_of_niftyAll)
+
+            if(last_day_of_niftyAll == last_day_of_nifty50):
+                easygui.msgbox("Data Already Present in Nifty50Derived table", title="Process Message")
+                pass
+            else:
+                print("Entered")
+                sql_select_query = "Select * from NiftyAll  where Symbol = (Select DISTINCT SYMBOL FROM NIFTY50 " \
+                                   "WHERE NIFTY50.SYMBOL = NIFTYALL.SYMBOL)  and Series='EQ' and timestamp  " \
+                                   "between '"+str(last_day_of_nifty50)+"' and '" + str(last_day_of_niftyAll)+"'"
+
+                print(sql_select_query)
+
+                cursor.execute(sql_select_query)
+                records = cursor.fetchall()
+
+                # Loop to store data
+                for i in records:
+                    cursor.execute(""" INSERT INTO Nifty50Derived VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                                   (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12]))
+
+                connection.commit()
+                easygui.msgbox("Record inserted successfully into Nifty50Derived table", title="Process Message")
+
         except mysql.connector.Error as error:
             connection.rollback()  # rollback if any exception occured
             msg="Failed inserting record into Nifty50Derived table {}".format(error)
