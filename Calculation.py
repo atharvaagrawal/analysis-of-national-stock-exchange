68b1bb405c6a76ef207699f9383ec83d877c449a
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2019-06-27 17:57:30 +0530

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -1,5 +1,10 @@
 import easygui
+import configparser
+
 class CalculationClass:
+    config_obj = configparser.ConfigParser()
+    config_obj.read("Y:\\Python CSV\\1 Main Technical Analysis of National Stock Exchange\\Config\\Config.cfg")
+    
     def calculatingData(self):
         easygui.msgbox("Hello From Calculation", title="Process Message")
         return
